e344a6a20bc000ba66bf6cab5e8654b530598305
itsneo-2003 <sarveshnagothi01@gmail.com>
2026-04-29 14:09:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,3 +14,46 @@ $Zone1OUs = Get-ADOrganizationalUnit -Filter * -SearchBase "DC=zone1,DC=scbdev,D
     }
 
 $Zone1OUs | Sort-Object | Get-Unique
+
+
+
+
+
+$SyncConnector = Get-ADSyncConnector | Where-Object { $_.Name -notmatch ' - aad' }
+$Zone2Partition = $SyncConnector.Partitions | Where-Object { $_.Name -match "zone2" }
+
+$IncludedOUs = $Zone2Partition.ConnectorPartitionScope.ContainerInclusionList | Where-Object { $_ -match "^OU=" }
+$ExcludedOUs = $Zone2Partition.ConnectorPartitionScope.ContainerExclusionList
+
+$Zone2OUs = Get-ADOrganizationalUnit -Filter * -SearchBase "DC=zone2,DC=scbdev,DC=net" -SearchScope Subtree -Server "zone2.scbdev.net" |
+    Select-Object -ExpandProperty DistinguishedName |
+    Where-Object {
+        $current = $_
+        $isExcluded = $ExcludedOUs | Where-Object { $current -eq $_ -or $current -like "*,$_" }
+        $isIncluded = $IncludedOUs | Where-Object { $current -eq $_ -or $current -like "*,$_" }
+        -not $isExcluded -or $isIncluded
+    }
+
+$Zone2OUs | Sort-Object | Get-Unique
+
+
+
+
+
+
+$SyncConnector = Get-ADSyncConnector | Where-Object { $_.Name -notmatch ' - aad' }
+$Zone3Partition = $SyncConnector.Partitions | Where-Object { $_.Name -match "zone3" }
+
+$IncludedOUs = $Zone3Partition.ConnectorPartitionScope.ContainerInclusionList | Where-Object { $_ -match "^OU=" }
+$ExcludedOUs = $Zone3Partition.ConnectorPartitionScope.ContainerExclusionList
+
+$Zone3OUs = Get-ADOrganizationalUnit -Filter * -SearchBase "DC=zone3,DC=scbdev,DC=net" -SearchScope Subtree -Server "zone3.scbdev.net" |
+    Select-Object -ExpandProperty DistinguishedName |
+    Where-Object {
+        $current = $_
+        $isExcluded = $ExcludedOUs | Where-Object { $current -eq $_ -or $current -like "*,$_" }
+        $isIncluded = $IncludedOUs | Where-Object { $current -eq $_ -or $current -like "*,$_" }
+        -not $isExcluded -or $isIncluded
+    }
+
+$Zone3OUs | Sort-Object | Get-Unique
